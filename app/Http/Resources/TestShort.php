e546c68e887c450508057b77e37295d9a4a3f0fb
edotovmasyan <edotovmasyan21@gmail.com>
2020-05-25 15:24:33 +0400

    add description to testshort rss

--- app/Http/Resources/TestShort.php
+++ app/Http/Resources/TestShort.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Str;
 
 class TestShort extends JsonResource
 {
@@ -19,6 +20,7 @@ class TestShort extends JsonResource
             'title' => $this->title,
             'subject' => TesteeSubjectShort::make($this->subject),
             'group' => TesteeGroupShort::make($this->group),
+            'description' => Str::limit($this->description, 50, '...'),
             'start' => $this->start,
             'end' => $this->end,
             'created_at' => $this->created_at,
